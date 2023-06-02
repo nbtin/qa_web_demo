19317c5ae408635314566edbd7100cff6eac3633
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2023-06-03 01:31:17 +0700

    Fix bug Docker.

--- app/vqa/models/blip.py
+++ app/vqa/models/blip.py
@@ -22,7 +22,7 @@ from timm.models.hub import download_cached_file
 
 class BLIP_Base(nn.Module):
     def __init__(self,                 
-                 med_config = '../configs/med_config.json',  
+                 med_config = './configs/med_config.json',  
                  image_size = 224,
                  vit = 'base',
                  vit_grad_ckpt = False,
