842fc586c21c9150ad55a2db7a92d3153c63fded
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2023-07-15 10:31:15 +0700

    Fix API_TOKEN.

--- app/vqa/utils.py
+++ app/vqa/utils.py
@@ -131,7 +131,7 @@ import requests
 
 API_URL = "https://api-inference.huggingface.co/models/distilbert-base-cased-distilled-squad"
 API_TOKEN = "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"
-headers = {"Authorization": f"Bearer "}
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
